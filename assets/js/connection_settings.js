5ad78cddcd84aef85ebef808b5687ba52f39be66
Alexander Minza <alexander.minza@gmail.com>
2026-01-30 11:07:55 +0200

    connecton_settings jQuery

--- assets/js/connection_settings.js
+++ assets/js/connection_settings.js
@@ -1,20 +1,20 @@
 /* global maib_connection_settings */
-jQuery(function () {
-    var maib_connection_basic_fields = jQuery(maib_connection_settings.connection_basic_fields_ids).closest("tr");
-    var maib_connection_advanced_fields = jQuery(maib_connection_settings.connection_advanced_fields_ids).closest("tr");
+jQuery(function ($) {
+    var $connectionBasicFields = $(maib_connection_settings.connection_basic_fields_ids).closest('tr');
+    var $connectionAdvancedFields = $(maib_connection_settings.connection_advanced_fields_ids).closest('tr');
 
-    maib_connection_basic_fields.hide();
-    maib_connection_advanced_fields.hide();
+    $connectionBasicFields.hide();
+    $connectionAdvancedFields.hide();
 
-    jQuery(maib_connection_settings.basic_settings_button_id).on("click", function (e) {
+    $(maib_connection_settings.basic_settings_button_id).on('click', function (e) {
         e.preventDefault();
-        maib_connection_advanced_fields.hide();
-        maib_connection_basic_fields.show();
+        $connectionAdvancedFields.hide();
+        $connectionBasicFields.show();
     });
 
-    jQuery(maib_connection_settings.advanced_settings_button_id).on("click", function (e) {
+    $(maib_connection_settings.advanced_settings_button_id).on('click', function (e) {
         e.preventDefault();
-        maib_connection_basic_fields.hide();
-        maib_connection_advanced_fields.show();
+        $connectionBasicFields.hide();
+        $connectionAdvancedFields.show();
     });
 });
