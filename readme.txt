9863786062a3fcaeb0a6c684be40df5d857b2dc6
Alexander Minza <alexander.minza@gmail.com>
2026-07-28 19:57:22 +0300

    version bump

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: alexminza
 Tags: Moldova, maib, Visa, Mastercard, payment gateway
 Requires at least: 4.8
-Tested up to: 6.9
-Stable tag: 1.5.1
+Tested up to: 7.0
+Stable tag: 1.5.2
 Requires PHP: 7.2.5
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
--- wc-moldovaagroindbank.php
+++ wc-moldovaagroindbank.php
@@ -4,7 +4,7 @@
  * Plugin Name: Payment Gateway for maib for WooCommerce
  * Description: Accept Visa and Mastercard directly on your store with the Payment Gateway for maib for WooCommerce.
  * Plugin URI: https://github.com/alexminza/wc-moldovaagroindbank
- * Version: 1.5.1
+ * Version: 1.5.2
  * Author: Alexander Minza
  * Author URI: https://profiles.wordpress.org/alexminza
  * Developer: Alexander Minza
@@ -15,9 +15,9 @@
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Requires PHP: 7.2.5
  * Requires at least: 4.8
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * WC requires at least: 3.3
- * WC tested up to: 10.5.3
+ * WC tested up to: 10.9.4
  * Requires Plugins: woocommerce
  *
  * @package wc-moldovaagroindbank
